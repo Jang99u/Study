a779aa5ebd8e766ad6a03e3ef91b7fef3dc796af
Github <alswkdrb@dgu.ac.kr>
2023-07-05 21:17:59 +0900

    BOJ_20044

---
+++ 20044.py
@@ -0,0 +1,10 @@
+Team = int(input(""))
+N = list(map(int, input("").split()))
+N.sort()
+
+result = []
+for i in range(Team) :
+    Score = N[i] + N[-(i+1)]
+    result.append(Score)
+
+print(min(result))
