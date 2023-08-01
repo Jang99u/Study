91000487c7b86592f8191f3f0c9f5b890a58d9f5
Github <alswkdrb@dgu.ac.kr>
2023-08-01 20:27:06 +0900

    BOJ_1038

---
+++ 1038.py
@@ -0,0 +1,21 @@
+strnumlst = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+lst = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+numlst = [0,1,2,3,4,5,6,7,8,9]
+result = lst
+
+while len(numlst) != 0 :
+    inputlst = []
+    for j in numlst :
+        k = 0
+        while lst[k][0] != str(j) :
+            inputlst.append(strnumlst[j] + lst[k])
+            k += 1
+    result.extend(inputlst)
+    lst = inputlst
+    numlst.pop(0)
+
+N = int(input())
+try :
+    print(int(result[N]))
+except :
+    print(-1)
