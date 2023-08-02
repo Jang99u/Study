adc81c709bc08356e913271eab9dde05659978dd
Github <alswkdrb@dgu.ac.kr>
2023-08-02 16:59:11 +0900

    .

--- Crawling/exam/ex01.py
+++ Crawling/exam/ex01.py
@@ -1,3 +1,5 @@
+# https://auto-trading.tistory.com/entry/%ED%8A%B9%EA%B0%95-%EC%9B%B9%ED%81%AC%EB%A1%A4%EB%A7%81Web-Crowling-%EA%B8%B0%EC%B4%88-%EA%B0%9C%EB%85%90%EA%B3%BC-%EC%BD%94%EB%93%9C-%EA%B5%AC%ED%98%84with-Python
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
