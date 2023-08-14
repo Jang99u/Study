1c963da5fbaf95c277a714e7c985eba36686d550
Github <alswkdrb@dgu.ac.kr>
2023-08-14 20:03:31 +0900

    BOJ_1010

--- 1010.py
+++ 1010.py
@@ -2,15 +2,21 @@ import sys
 from itertools import combinations 
 input = sys.stdin.readline
 
+def factorial(n) :
+    num = 1
+    while n > 0 :
+        num *= n
+        n -= 1
+        
+    return num
+        
 def nCr(n, r) :
-    n = [i for i in range(n)]
-    
-    return len(list(combinations(n, r)))
+    return int((factorial(n) / factorial(n-r)) / factorial(r))
 
 TC = int(input())
 result = []
 for _ in range(TC) :
     r, n = map(int, input().split())
-    result.append(nCr(n, r))
+    result.append(str(nCr(n, r)))
     
-print(result)
+print("\n".join(result))
