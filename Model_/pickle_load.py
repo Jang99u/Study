d8ff14384d728dfdaddca1c66fda101062a56eb5
Github <alswkdrb@dgu.ac.kr>
2023-08-16 12:51:11 +0900

    .

--- Model_/pickle_load.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# 피클 파일 로드
-with open('./NLP/Model_/emotion_analyzer.pkl', 'rb') as f:
-    loaded_analyzer = pickle.load(f)
-
-# 감정 분석 수행 및 결과 출력
-result = loaded_analyzer.analyze_emotion()
--- Model_/pickle_make.py
+++
@@ -1,7 +0,0 @@
-import pickle
-from emotion_object import EmotionAnalysis
-
-analyzer = EmotionAnalysis()
-# 피클 파일로 저장
-with open('./NLP/Model_/emotion_analyzer.pkl', 'wb') as f:
-    pickle.dump(analyzer, f)
