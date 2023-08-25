ecdd4ddab8888991166e952f6085db0df7adc306
Github <alswkdrb@dgu.ac.kr>
2023-08-25 22:58:15 +0900

    BOJ_1049

--- 1049.py
+++ 1049.py
@@ -1,20 +1,32 @@
-n, m = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-answer = 0
-price_list = []
+def BuyString(num, cost) :
+    global lst1, lst6
+    if num >= 6 :
+        return num-6, cost+lst6[0][0]
+    else : 
+        if num*lst1[0][1] <= lst6[0][0] :
+            return 0, cost+(num*lst1[0][1])
+        else :
+            return 0, cost+lst6[0][0]
 
-for _ in range(m):
-    price = tuple(map(int, input().split()))
-    price_list.append(price)
+N, M = map(int, input().split())
+store = []
 
-six_list = sorted(price_list, key=lambda x : x[0])
-one_list = sorted(price_list, key=lambda x : x[1])
+for i in range(M) :
+    store.append(list(map(int, input().split())))
+    
+lst6 = sorted(store, key = lambda x : x[0]) # 6개 가격 기준 정렬
+lst1 = sorted(store, key = lambda x : x[1]) # 1개 가격 기준 정렬
+# 정렬 후 리스트로 끌고 다니지 말고 6개 기준 가장 싼 가격 / 1개 기준 가장 싼 가격 이렇게 2개만 필요함
 
-if six_list[0][0] <= one_list[0][1] * 6:
-    answer = six_list[0][0] * (n // 6) + one_list[0][1] * (n % 6)
-    if six_list[0][0] < one_list[0][1] * (n % 6):
-        answer = six_list[0][0] * (n//6 + 1)
-else:
-    answer = one_list[0][1] * n
+if lst1[0][1] * 6 <= lst6[0][0] :
+    print(lst1[0][1] * N)
+    exit(0)
+else :
+    C = 0
+    while N > 0 :
+        N, C = BuyString(N, C)
 
-print(answer)
+print(C)
