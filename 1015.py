94bf4cefe875f55ac48eafff0f700e42d5526ba9
Github <alswkdrb@dgu.ac.kr>
2023-09-19 15:45:18 +0900

    BOJ_1015

--- 1015.py
+++ 1015.py
@@ -1,9 +1,11 @@
-n = int(input())
-t = list(map(int, input().split()))
-s_li = sorted(t)
-li = []
-for i in range(n):
-    idx = s_li.index(t[i])
-    li.append(idx)
-    s_li[idx] = -1
-print(*li)
+N = int(input())
+lst = list(map(int, input().split()))
+for i in range(len(lst)) :
+    lst[i] = [lst[i], i]    
+lst = sorted(lst, key = lambda x : x[0])
+for i in range(len(lst)) :
+    lst[i].append(i)
+lst = sorted(lst, key = lambda x : x[1])
+
+for i in lst :
+    print(i[2], end = " ")
