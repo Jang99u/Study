aa43e0224604682fa93393ced11a90396554fa6e
Github <alswkdrb@dgu.ac.kr>
2023-10-30 19:55:10 +0900

    Lecture_1

---
+++ Lecture_1/E1.12/src/ThreeFavorites.java
@@ -0,0 +1,9 @@
+public class ThreeFavorites
+{
+    public static void main(String[] args)
+    {
+        System.out.println("About Time");
+        System.out.println("Begin Again");
+        System.out.println("La La Land");
+    }    
+}
